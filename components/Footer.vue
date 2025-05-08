0b5c4e879fe3c91d8b756b896d8bb17f4ff87050
dzulkifli578 <dzulkiflianwar2@gmail.com>
2025-05-08 08:52:25 +0700

    refactor: make footer simpler

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="py-12 text-white bg-gray-900">
+    <footer class="py-12 text-white border-t border-gray-400 dark:bg-gray-900 bg-gray-50">
         <div class="container px-6 mx-auto">
             <div class="flex flex-col items-center justify-between md:flex-row">
                 <div class="mb-6 md:mb-0">
@@ -7,18 +7,9 @@
                     <p class="mt-2 text-gray-400">© {{ new Date().getFullYear() }} Dzulkifli Anwar. All rights reserved.
                     </p>
                 </div>
-
-                <div class="flex space-x-6">
-                    <a href="#home" class="text-gray-400 transition hover:text-white">Home</a>
-                    <a href="#about" class="text-gray-400 transition hover:text-white">About</a>
-                    <a href="#skills" class="text-gray-400 transition hover:text-white">Skills</a>
-                    <a href="#projects" class="text-gray-400 transition hover:text-white">Projects</a>
-                    <a href="#contact" class="text-gray-400 transition hover:text-white">Contact</a>
+                <div class="flex items-center">
+                    <p class="text-gray-400">Crafted with precision by DeepSite • Powered by Nuxt 3</p>
                 </div>
-            </div>
-
-            <div class="pt-8 mt-8 text-center text-gray-400 border-t border-gray-800">
-                <p>Crafted with precision by DeepSite • Powered by Nuxt 3 </p>
             </div>
         </div>
     </footer>
